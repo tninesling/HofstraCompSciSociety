cbb6ddca718d037dcec08208b1d58eb6821d69b7
Taylor Ninesling <t9sling@hotmail.com>
2015-07-14 21:25:14 -0400

    Stylesheet links corrected

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="CompSciSociety/custom.css">
+	<link rel="stylesheet" type="text/css" href="custom.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	<script src="CompSciSociety/custom-script.js" type="text/javascript"></script>
+	<script src="custom-script.js" type="text/javascript"></script>
 </head>
 <body>
 	<!-- Header -->
